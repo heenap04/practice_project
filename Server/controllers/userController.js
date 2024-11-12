4529864e55522da01329618f6c52b346110dfa50
Heena P <pawriyah2004@gmail.com>
2024-11-12 11:44:29 +0530

    get post account details

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,7 +1,76 @@
+// const asyncHandler = require("express-async-handler");
+// const bcrypt = require("bcrypt");
+// const User = require("../model/userModel");
+
+// const registerUser = asyncHandler(async (req, res) => {
+//     const { firstName, lastName, age, gender, bloodGroup, email, phoneNumber, password } = req.body;
+
+//     // Validate required fields
+//     if (!firstName || !lastName || !age || !gender || !bloodGroup || !email || !phoneNumber || !password) {
+//         res.status(400);
+//         throw new Error("Please fill all fields");
+//     }
+
+//     // Check if user already exists
+//     const userExists = await User.findOne({ email });
+//     if (userExists) {
+//         return res.status(400).json({ message: "User already exists" });
+//     }
+
+//     // Hash the password
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+//     // Create a new user
+//     const newUser = await User.create({
+//         firstName,
+//         lastName,
+//         age,
+//         gender,
+//         bloodGroup,
+//         email,
+//         phoneNumber,
+//         password: hashedPassword,
+//     });
+
+//     res.status(201).json({ message: "User registered successfully", user: newUser });
+// });
+
+// const loginUser = asyncHandler(async (req, res) => {
+//     const { email, password } = req.body;
+//     if (!email || !password) {
+//         res.status(400);
+//         throw new Error("Please provide email and password");
+//     }
+//     const user = await User.findOne({ email });
+//     if (user && (await bcrypt.compare(password, user.password))) {
+//         res.status(200).json({
+//             message: "User logged in successfully",
+//             user: {
+//                 id: user._id,
+//                 firstName: user.firstName,
+//                 lastName: user.lastName,
+//                 email: user.email,
+//             },
+//         });
+//     } else {
+//         res.status(401);
+//         throw new Error("Invalid email or password");
+//     }
+// });
+
+// module.exports = { registerUser, loginUser };
+
+
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken"); // Import jsonwebtoken
 const User = require("../model/userModel");
 
+// Secret key for JWT (You might want to keep this in environment variables)
+const JWT_SECRET = "hey2004abc"; // Replace this with a secure, environment-specific secret key
+
+// Register user
 const registerUser = asyncHandler(async (req, res) => {
     const { firstName, lastName, age, gender, bloodGroup, email, phoneNumber, password } = req.body;
 
@@ -36,27 +105,26 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({ message: "User registered successfully", user: newUser });
 });
 
+// Login user with dynamic JWT token
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(400);
-        throw new Error("Please provide email and password");
-    }
+
+    // Check for user
     const user = await User.findOne({ email });
-    if (user && (await bcrypt.compare(password, user.password))) {
-        res.status(200).json({
-            message: "User logged in successfully",
-            user: {
-                id: user._id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-            },
-        });
-    } else {
-        res.status(401);
-        throw new Error("Invalid email or password");
+    if (!user) {
+        return res.status(400).json({ message: "Invalid credentials" });
     }
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" }); // Token expires in 1 hour
+
+    res.json({ message: "Login successful", token });
 });
 
 module.exports = { registerUser, loginUser };
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const jwtAuthMiddleware  = require("../middlewares/jwtMiddleware").validateJwtToken;
+const jwtAuthMiddleware = require("../middlewares/jwtMiddleware").validateJwtToken;
 
 const { registerUser, loginUser } = require("../controllers/userController");
+
+// My Account function to get user details by email
+const myAccount = async (req, res) => {
+    try {
+        const userEmail = req.user.email; // Get email from JWT token payload
+        const user = await User.findOne({ email: userEmail });  // Find user by email
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.json({
+            id: user._id,
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.email,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
 
 // Route to register a new user
 router.post("/register", registerUser);
@@ -10,6 +31,11 @@ router.post("/register", registerUser);
 // Route to login a user
 router.post("/login", loginUser);
 
-router.post("/login",jwtAuthMiddleware,loginUser);
+// Route to login a user with JWT middleware
+router.post("/login", jwtAuthMiddleware, loginUser);
+
+// Protected routes to access user account details by email
+router.get("/my-account", jwtAuthMiddleware, myAccount);
+router.post("/my-account", jwtAuthMiddleware, myAccount);
 
 module.exports = router;
